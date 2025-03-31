215a0b0013be1a95ba6820521ea5f577b0cc84bf
cveattack <87832743+cveattack@users.noreply.github.com>
2025-03-31 06:57:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import sympy as sp
+
+def find_derivative(expr_str, var_str):
+    x = sp.symbols(var_str)
+    expr = sp.sympify(expr_str)
+    der = sp.diff(expr, x)
+
+    return der
+
+def main():
+    expr_str = input("Введите выражение: ").strip()
+    var_str = input("Введите переменную: ").strip()
+
+    try:
+        print(f"Производная выражения {expr_str} по переменной {var_str} равна: \n{find_derivative(expr_str, var_str)}")
+
+        
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+if __name__ == "__main__":
+    main()
